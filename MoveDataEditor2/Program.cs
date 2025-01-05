a1f6d14b98743cbebacb708f8803abf0e3d469e3
BetaRelics <131507020+BetaRelics@users.noreply.github.com>
2025-01-05 01:06:44 -0500

    Overhauled the file locating and also added code that copies the target stf_rom folder before modifying it

--- MoveDataEditor2/Program.cs
+++ MoveDataEditor2/Program.cs
@@ -1,26 +1,50 @@
 ﻿
-//It's just occured to me that we should probably be using the ep file for this
-//hm, actually we need to use both the data and ep files, huh
+//we need to use both the data and ep files, huh
 //maybe we can make it depend on how large the value entered is?
-//also, we should probably ask the user if they want to first copy their rom to a new directory before editing
-//that way no one unintentionally overwrites their rom
-
+Start:
+string RomPath;
 string DataFilePath;
-Console.WriteLine("Enter Data File Path");
-DataFilePath = Console.ReadLine();
+string EpFilePath;
+Console.WriteLine($"Just a heads up, this application will make a back-up of your current rom folder");
+Console.WriteLine($"It'll be stored in a folder in the same directory as this application");
+Console.WriteLine($"I'd keep in mind whether your stf_rom folder is modded or not");
+Console.WriteLine("Enter stf_rom folder Path");
+Console.WriteLine(Directory.GetCurrentDirectory());
+RomPath = Console.ReadLine();
 
-if (DataFilePath == null)
+if (RomPath == null)
 {
     Console.WriteLine("null value entered. Why would you do that?");
     return;
 }
 
 //trims quotes from path if there are any
-DataFilePath = DataFilePath.Trim('"');
-Console.WriteLine(DataFilePath);
+RomPath = RomPath.Trim('"');
+Console.WriteLine(RomPath);
 
+//Console.WriteLine($"Do you wish to make a copy of the original folder before you make any changes?");
+//if (Console.ReadLine() == "y")
 
+string FolderForBackup = Path.Combine(Directory.GetCurrentDirectory(), $"TempRomBackUp");
+var dir = new DirectoryInfo(RomPath);
+if (!dir.Exists)
+{
+    Console.WriteLine($"The Entered folder doesn't seem to exist, here's what was entered:{dir.FullName}");
+    Console.WriteLine($"Press any key and hit enter to start the program from the beginning again");
+    Console.ReadLine();
+    goto Start;
+}
 
+DirectoryInfo[] dirs = dir.GetDirectories();
+Directory.CreateDirectory(FolderForBackup);
+foreach (FileInfo file in dir.GetFiles())
+{
+    string TargetFilePath = Path.Combine(FolderForBackup, file.Name);
+    file.CopyTo( TargetFilePath );
+}
+
+DataFilePath = Path.Combine(RomPath, $"rom_data.bin");
+EpFilePath = Path.Combine(RomPath, $"rom_ep.bin");
 
 
 DataFilePath = Path.GetFullPath(DataFilePath);
@@ -61,7 +85,7 @@ using (FileStream fs = File.Open(DataFilePath, FileMode.Open, FileAccess.Read, F
         {
             Console.WriteLine($"Negative ID entered, try entering a move ID that exists/is positive");
         }
-       
+
         fs.Seek(CalculatedOffset + 0x120004, SeekOrigin.Begin);
     }
     catch (Exception e)
@@ -120,7 +144,8 @@ using (FileStream fs = File.Open(DataFilePath, FileMode.Open, FileAccess.Read, F
     while (ConvertedBuffer != 1)
     {
 
-
+        //somewhere here we might include some code that checks for how many keyframes are in each set
+        //since there are bytes right before the keyframe floats that specify that
         fs.ReadExactly(FloatBuffer, 0, 4);
         ConvertedBuffer = BitConverter.ToSingle(FloatBuffer, 0);
         string FloatBufferString = Convert.ToHexString(FloatBuffer);
@@ -134,6 +159,7 @@ using (FileStream fs = File.Open(DataFilePath, FileMode.Open, FileAccess.Read, F
 
     //Move the Filestream position back 4 bytes so we include the first keyframe in the list
     fs.Position = (fs.Position - 4);
+    long FloatStartPos = fs.Position;
     List<int> UniqueKeyFrames = new List<int>();
 
     //the while condition here runs unless the float buffer returns a non-integer value
@@ -158,13 +184,13 @@ using (FileStream fs = File.Open(DataFilePath, FileMode.Open, FileAccess.Read, F
         }
 
         int BufferToInt = (int)ConvertedBuffer;
-        
+
         //here we're making a list of the unique keyframes
         //in the future the user will be able to view them and will have the option to replace them
         //this isn't super customizable but it's good for simple framedata changes
         if (!UniqueKeyFrames.Contains(BufferToInt))
         {
-        UniqueKeyFrames.Add(BufferToInt);
+            UniqueKeyFrames.Add(BufferToInt);
         }
 
 
@@ -173,8 +199,46 @@ using (FileStream fs = File.Open(DataFilePath, FileMode.Open, FileAccess.Read, F
     //string DefaultF = BitConverter.ToString(DefaultFloat);
     //Console.WriteLine(DefaultF);
 
+
+    UniqueKeyFrames.ForEach(i => Console.WriteLine(i + ","));
+    List<int> NewKeyFramesList = new List<int>();
+    //string NewList;
+    int CurrentKeyFrame = 1;
+    Console.WriteLine($"Please enter what you'd like to swap these frame timings to");
+    Console.WriteLine($"Enter one number at a time, in the order they appeared in before");
+    //Console.WriteLine($"Make sure the);
+
+    foreach (int KeyFrame in UniqueKeyFrames)
+    {
+        try
+        {
+            Console.WriteLine($"Enter keyframe {CurrentKeyFrame}");
+            string NewKeyFrame = Console.ReadLine();
+            if (NewKeyFrame == null)
+            {
+                return;
+            }
+            int KeyFrameForReplace = Convert.ToInt16(NewKeyFrame);
+            NewKeyFramesList.Add(KeyFrameForReplace);
+            ++CurrentKeyFrame;
+        }
+
+        catch
+        {
+            Console.WriteLine($"something went wrong, failed to read/replace keyframe");
+        }
+    }
+    //int OldListLength = UniqueKeyFrames.Count;
+    //int NewListLength = NewKeyFrames.Count;
+    if (UniqueKeyFrames.Count - NewKeyFramesList.Count != 0)
+    {
+        Console.WriteLine($"Error! List lengths do not match!");
+        return;
+    }
     
-    UniqueKeyFrames.ForEach(i => Console.WriteLine(i + "," ));
-    
+    fs.Seek(FloatStartPos, SeekOrigin.Begin);
+
+
 
 }
+    
